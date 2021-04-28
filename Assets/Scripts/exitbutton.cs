4f763383f9fb949f58c8421e907219f63cc3f267
Emerast Green <kamil_sienkowski777@gmail.com>
2021-04-28 10:58:10 +0200

    Restrukturyzacja folderów
    Utworzono foldery: Model, Music, Prefabs, Scripts oraz uporządkowano pliki.

---
+++ Assets/Scripts/exitbutton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitbutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseOver()
+    {
+        print("Hovering2");
+    }
+    void OnMouseDown()
+    {
+        Application.Quit();
+        print("Clicked2!");
+    }
+}
