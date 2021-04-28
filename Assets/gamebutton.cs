c7247cc1c6326b300e21de5589bc225989fe82ed
Emerast Green <kamil_sienkowski777@gmail.com>
2021-04-28 10:20:18 +0200

    Initial Commit
    Version 1.0

---
+++ Assets/gamebutton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamebutton : MonoBehaviour
+{
+    public gamescript gamescript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseOver()
+    {
+        print("Hovering");
+    }
+    void OnMouseDown()
+    {
+        print("Clicked!");
+        gamescript.menu.planeDistance = -1;
+        gamescript.game.planeDistance = 60;
+        gamescript.stage = 1;
+        gamescript.GameStart();
+    }
+}
